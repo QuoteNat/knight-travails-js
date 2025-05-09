310c82bbcda72b82429630455eaa3d651dd193ec
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2025-05-09 13:57:51 -0500

    Add printPath()

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 import { knightMoves } from "./knight.js";
 
-knightMoves([3, 3], [4, 3]);
+function printPath(path) {
+  console.log(
+    `The knight made it in ${path.length - 1} moves! Here is the path:`
+  );
+  for (const coord of path) {
+    console.log(JSON.stringify(coord));
+  }
+}
+
+let path = knightMoves([0, 0], [3, 3]);
+printPath(path);
